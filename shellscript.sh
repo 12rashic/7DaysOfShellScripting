ce5d26578f85c7c5d8d63cc9b5e8b492a139d0c3
Rashi Chaubal <12rashic@gmail.com>
2023-08-01 22:58:08 +0530

    Day 1 of shell Scripting

---
+++ shellscript.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+echo " Welcome to BashBlaze 7 Days of Bash Scripting Challenge 🥳 "
+
+#This is demonstration of Variable in shell scripting
+Var='Hello August ☂'
+
+echo " $Var"
+
+echo " This is $PWD "
+echo " This is $HOME"
+echo " This is $PPID"
+
+ls -l  #
